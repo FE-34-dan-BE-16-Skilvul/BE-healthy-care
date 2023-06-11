5530a6b80d304a8c980f979891219c34bd1bde4e
Arisandi <arisandisatria541@gmail.com>
2023-06-11 20:05:15 +0700

    delete categories route and controller in dev

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -3,7 +3,9 @@ const { foods, categories } = require("../models/index");
 const foodController = {
   getAllFoods: async (req, res) => {
     try {
-      const allFoods = await foods.findAll();
+      const allFoods = await foods.findAll({
+        include: [categories],
+      });
 
       if (!allFoods) {
         return res.status(404).json({
@@ -11,10 +13,20 @@ const foodController = {
         });
       }
 
+      const result = allFoods.map((food) => {
+        return {
+          name: food.name,
+          img: food.img,
+          calory: food.calory,
+          nutrition: food.nutrition,
+          category_name: food.category.name,
+        };
+      });
+
       return res.json({
         status: 200,
         message: "success",
-        data: allFoods,
+        data: result,
       });
     } catch (error) {
       return res.status(500).json({
