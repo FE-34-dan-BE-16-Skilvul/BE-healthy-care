a8b1bdd636b1d2fd50555c93387faf0403ec4e5e
Arisandi <arisandisatria541@gmail.com>
2023-06-15 10:53:23 +0700

    added authentication for bmi route

--- routes/bmiRouter.js
+++ routes/bmiRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bmiController = require("../controllers/bmiMainController");
+const authMiddleware = require("../middleware/auth");
 const bmiRouter = express.Router();
+
+bmiRouter.use(authMiddleware);
 
 //id_user
 bmiRouter.post("/bmi/:id", bmiController.insertBmi);
@@ -8,6 +11,5 @@ bmiRouter.get("/bmi/:id", bmiController.getBmi);
 
 //id_bmi
 bmiRouter.delete("/bmi/:id", bmiController.deleteBmi);
-
 
 module.exports = bmiRouter;
