818d76d4a3a1ad5d391217f1f26e37dcec4fc4c5
Arisandi <arisandisatria541@gmail.com>
2023-06-16 09:13:06 +0700

    delete reqiure for daily routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./users");
 const bmiRouter = require("./bmiRouter");
-const dailyRouter = require("./dailyRouter");
 const foodRouter = require("./foods");
 const articleRouter = require("./article");
 
